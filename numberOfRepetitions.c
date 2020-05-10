21f42c2380835cb7002ca5fc8eaf08d74eba291d
islam <islam.elsayed101@gmail.com>
2020-05-11 01:53:39 +0200

    this program count how many number is exist in a sorted array

---
+++ numberOfRepetitions.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+typedef enum
+{
+    SEARCH_FIRST,
+    SEARCH_LAST
+}searchType;
+
+int binarySearch(int, int [], int, searchType);
+
+int main()
+{
+    int arr[] = {1,5,5,5,7,7,7,7,7,7,9,9,9,9,55}, firstIndex, lastIndex, count, size, target = 9;
+    size = sizeof(arr)/sizeof(arr[0]);
+    firstIndex = binarySearch(target,arr,size,SEARCH_FIRST);
+    lastIndex = binarySearch(target,arr,size,SEARCH_LAST);
+    count = (lastIndex - firstIndex) + 1;
+    if(firstIndex > 0)
+        printf("number %d is exist %d times\n",target,count);
+    else
+        printf("number %d not found",target);
+    return 0;
+}
+
+int binarySearch(int targetNumber, int arr[], int size , searchType type)
+{
+    int mid, resulat = -1, start = 0, end = size-1;
+    while(start <= end)
+    {
+        mid = start + (end - start)/2;
+        if(targetNumber == arr[mid])
+        {
+            resulat = mid;
+            if(type == SEARCH_FIRST)
+                end = mid - 1;
+            else
+                start = mid+1;
+        }
+        else if(targetNumber > arr[mid])
+            start = mid+1;
+        else
+            end = mid-1;
+    }
+    return resulat;
+}
